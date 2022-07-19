cc100313327c5d933fed0dc39b03374fdbb0ee98
Yhya Nesb <yhya.syrian@gmail.com>
2022-07-19 17:58:15 +0300

    test1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd2190a44d4e22f24a61f3d2f4030f09d75f113c2',
+        'name' => 'yhyasyrian/sydb',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'yhyasyrian/sydb' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd2190a44d4e22f24a61f3d2f4030f09d75f113c2',
+            'dev_requirement' => false,
+        ),
+    ),
+);
