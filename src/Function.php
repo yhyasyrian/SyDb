94ff7e2bb79ff9da108d484f43638f8500037ef7
Yhya Nesb <yhya.syrian@gmail.com>
2022-08-09 18:15:38 +0300

    Add Function endColumn() - startColumn()

--- src/Function.php
+++ src/Function.php
@@ -12,7 +12,7 @@ class FunctionSql{
     {
         return str_replace(['"',"'"],['&#34;','&#39;'],$string); 
     }
-        /**
+    /**
     * @param mixed $string
     * Any String
     *
@@ -23,9 +23,45 @@ class FunctionSql{
     {
         return str_replace(['&#34;','&#39;'],['"',"'"],$string); 
     }
+    /**
+    * @param array $array
+    * For Add Key And Value In Table
+    *
+    * @return bool
+    */
     public function addWhere(array $array) :bool
     {
         $this->whereGlobal = $array;
         return \true;
     }
+    /**
+    * @param string $name
+    * Name Column
+    * @param int $number
+    * Number Get Value
+    * @param int $start
+    * Start After $number * $start
+    *
+    * @return string
+    * Can You Use The String In Function select In Third Param For Get Rows Last
+    */
+    public function endColumn(string $name = 'Id',int $number = 10,int $start = 0){ // Start Form 10 To 0
+		$start *= $number;
+		return 'ORDER BY '.$name.' DESC LIMIT '.$start.','.$number;
+	}
+        /**
+    * @param string $name
+    * Name Column
+    * @param int $number
+    * Number Get Value
+    * @param int $start
+    * Start After $number * $start
+    *
+    * @return string
+    * Can You Use The String In Function select In Third Param For Get Rows Start
+    */
+	public function startColumn(string $name = 'Id',int $number = 10,int $start = 0){ // Start Form 0 To 10
+		$start *= $number;
+		return 'ORDER BY '.$name.' LIMIT '.$start.','.$number;
+	}
 }
