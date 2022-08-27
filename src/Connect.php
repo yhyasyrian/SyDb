aeecfda182797aacbea6561bf0ea451b6bd57d49
Yhya Nesb <yhya.syrian@gmail.com>
2022-08-27 19:36:21 +0300

    Upload File Dist (;

--- src/Connect.php
+++ src/Connect.php
@@ -12,16 +12,9 @@ class Connect extends Close{
     {
         try{
             if (empty($this->connect) or $this->timeStart < (\time() - $this->timeConnect)) {
-                \print_r([
-                    empty($this->connect),
-                    $this->timeStart < (\time() - $this->timeConnect),
-                    $this->timeStart,
-                    \time() - $this->timeConnect
-                ]);
                 $this->timeStart = \time();
-                $this->connect = 's';
-                /* @$this->connect = @mysqli_connect($this->Host,$this->User,$this->Pass,$this->DB);
-                @mysqli_set_charset($this->connect,$this->charset); */
+                @$this->connect = @mysqli_connect($this->Host,$this->User,$this->Pass,$this->DB);
+                @mysqli_set_charset($this->connect,$this->charset);
             }
         } catch (\mysqli_sql_exception $th) {
             throw new Exception($th->getMessage(),$th->getCode(),$th);
